4af8acd5d052a7da85f8f95d0165ed6b78dcc009
Jerimiah Mack <jerimiah.mack@gmail.com>
2018-04-11 15:19:12 -0400

    Enhance HomePage page object to conform to page object template concepts.
    Improved javadocs.
    Needed to implement Generic, internal to HomePage, in order to take
    advantage of contained methods (such as waiForElement()).

--- src/test/java/com/jmack/Base/PageObjects/HomePage.java
+++ src/test/java/com/jmack/Base/PageObjects/HomePage.java
@@ -1,32 +1,69 @@
 package com.jmack.Base.PageObjects;
 
+import com.jmack.Base.Generic;
 import com.jmack.Base.ScreenShot;
+import com.jmack.Base.TestBase;
 
 import io.qameta.allure.Step;
 
-public class HomePage {
 
-	private String testName = "unknown";
-	private String id = "unknown";
-	private ScreenShot ss;
+/**
+ * 
+ * @author Jerimiah Mack
+ *
+ */
+public class HomePage extends TestBase {
 	
-	public HomePage(ScreenShot ss, String id, String testName) {
+	private Generic generic;
+	private ScreenShot ss;
+	private String id = "unknown";
+	private String testName = "unknown";
+	
+	
+	/**
+	 * Minimum constructor
+	 * @param generic
+	 * @param ss
+	 */
+	public HomePage (Generic generic, ScreenShot ss) {
+		
+		this.generic = generic;
+		this.ss = ss;
+		
+	}
+	
+	
+	/**
+	 * Constructor provisioned for instance logging
+	 * @param generic
+	 * @param ss
+	 * @param id
+	 * @param testName
+	 */
+	public HomePage (Generic generic, ScreenShot ss, String id, String testName) {
+		
+		this.generic = generic;
 		this.ss = ss;
 		this.id = id;
 		this.testName = testName;
+		
 	}
 	
-	public HomePage(ScreenShot ss) {
-		this.ss = ss;
-	}
 	
 	/** 
 	 *  Do something with HomePage
+	 *  @param somthingToPass String (used only to change Allure pass/fail iconography from circle to arrow)
 	 */
 	@Step("Doing something with HomePage")
 	public void stuff(String somethingToPass) {
-		System.out.format("[LOG]: <[%s:%s] testing: \"%s\" on HomePage>%n", this.id, this.testName, somethingToPass);
 		
-		ss.assertTrue(true,"Some message if fail.");
+		System.out.format("[LOG]: <[%s:%s] testing: \"%s\" on HomePage>%n", id, testName, somethingToPass);
+		
+		ss.assertTrue(true,"Some message if fail."); // example of an assertion with screenshot on fail
+		//ss.takeScreenShot("Some description"); // example of taking screenshot, on demand
+		//generic.clickElement("Some.Property.Key"); // example of using generic methods
+		
 	}
+	
+	
 }
