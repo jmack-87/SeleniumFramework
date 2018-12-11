38b32f9ba44f0967812b73187ab02ab50f333236
Jerimiah Mack <jerimiah.mack@gmail.com>
2018-12-11 11:06:38 -0500

    log more stuff, default to gridType = local

--- src/test/java/com/jmack/Base/DataExtractor.java
+++ src/test/java/com/jmack/Base/DataExtractor.java
@@ -12,7 +12,7 @@ import com.google.gson.JsonParser;
 import io.qameta.allure.Step;
 
 public class DataExtractor {
-	
+
 	/*	Data available to Generic
 	 */
 	protected String sysOpt;
@@ -24,15 +24,15 @@ public class DataExtractor {
 	protected String resolution; //perfecto
 	protected String location; //perfecto
 	public String platform; //
-	
+
 	protected Boolean headless;
-	
+
 	protected String deviceName;
 	protected String model;
 	protected String appPackage;
 	protected String appActivity;
 	protected String bundleId;
-	
+
 	/*	Data available to scripts
 	 */
 	public String searchString;
@@ -45,52 +45,56 @@ public class DataExtractor {
 
 	private String id;
 	private String testName;
-	
+
 
 
 	/**
 	 * Extract and set test and system level data from reference JSON
+	 *
 	 * @param gc GlobalConstants instance
 	 * @param testName test class name String
+	 *
 	 * @throws FileNotFoundException in case gc.testDateFilePath invalid
 	 */
 	@Step("Extract dynamic test data from JSON.")
 	protected void initialize(GlobalConstants gc, String testName, String id) throws FileNotFoundException {
 		this.testName = testName;
 		this.id = id;
-		
+
 		Object[] data = getTestData(gc.testDataFilePath);
 		JsonArray testDataArray = (JsonArray) data[0]; // All test definitions, wrapped in Array
 		JsonArray systemDataArray = (JsonArray) data [1]; // All System Options definitions, wrapped in Array
-		
+
 		//System.out.format("[DEBUG]: <[%s:%s] TestsCount: %d>%n", this.id, this.testName, testDataArray.size());
 		//System.out.format("[DEBUG]: <[%s:%s] SystemDataCount: %d>%n", this.id, this.testName, systemDataArray.size());
-		
+
 		Iterator<JsonElement> sda = systemDataArray.iterator();
 		while (sda.hasNext()) { // there should only be one (1) array element, containing all key:value pairs
 			JsonObject sob = (JsonObject) sda.next();
 			//System.out.format("[DEBUG]: <[%s:%s] SystemData: %s>%n", this.id, this.testName, sob);
 			setSystemOptions(sob);
 		}
-		
+
 		Iterator<JsonElement> tda = testDataArray.iterator();
-		while (tda.hasNext()) { // there should only be one (1) array element, containing all test-arrays 
+		while (tda.hasNext()) { // there should only be one (1) array element, containing all test-arrays
 			JsonObject tob = (JsonObject) tda.next();
 			//System.out.format("[DEBUG]: <[%s:%s] TestsData: %s>%n", this.id, this.testName, tob);
 			setTestData(tob.getAsJsonObject(testName));
 		}
 	}
-	
+
+
 	/**
 	 * Set test-level data
+	 *
 	 * @param testData json array containing test level json elements
 	 */
 	@Step("Set Test data.")
 	private void setTestData(JsonObject testData) {
 		System.out.format("[LOG]: <[%s:%s] RuntimeData: %s>%n", this.id, this.testName, testData);
-		
-		this.gridType = testData.get("gridType") == null ? "" : testData.get("gridType").getAsString();
-		
+
+		this.gridType = testData.get("gridType") == null ? "local" : testData.get("gridType").getAsString();
+
 		this.platformName = testData.get("platformName") == null ? "" : testData.get("platformName").getAsString(); // perfecto
 		this.platformVersion = testData.get("platformVersion") == null ? "" : testData.get("platformVersion").getAsString(); // perfecto
 		this.browserName = testData.get("browserName") == null ? "" : testData.get("browserName").getAsString();
@@ -98,7 +102,7 @@ public class DataExtractor {
 		this.resolution = testData.get("resolution") == null ? "" : testData.get("resolution").getAsString(); // perfecto
 		this.location = testData.get("location") == null ? "" : testData.get("location").getAsString(); // perfecto
 		this.platform = testData.get("platform") == null ? "" : testData.get("platform").getAsString(); // perfecto
-		
+
 		this.headless = testData.get("headless") == null ? false : testData.get("headless").getAsBoolean(); // local
 
 		// mobile native
@@ -107,7 +111,7 @@ public class DataExtractor {
 		this.appPackage = testData.get("appPackage") == null ? "" : testData.get("appPackage").getAsString();
 		this.appActivity = testData.get("appActivity") == null ? "" : testData.get("appActivity").getAsString();
 		this.bundleId = testData.get("bundleId") == null ? "" : testData.get("bundleId").getAsString();
-		
+
 		this.searchString = testData.get("searchString") == null ? "IBM Perfecto" : testData.get("searchString").getAsString();
 		this.searchConfirmationString = testData.get("searchConfirmationString") == null ? "IBM" : testData.get("searchConfirmationString").getAsString();
 
@@ -116,22 +120,30 @@ public class DataExtractor {
 		this.creditCardNumber = testData.get("creditCardNumber") == null ? "" : testData.get("creditCardNumber").getAsString();
 		this.creditCardExpiration = testData.get("creditCardExpiration") == null ? "" : testData.get("creditCardExpiration").getAsString();
 		this.creditCardCVC = testData.get("creditCardCVC") == null ? "" : testData.get("creditCardCVC").getAsString();
+
+		System.out.format("[LOG]: <[%s:%s] Test data loaded.>%n", this.id, this.testName, testData);
+
 	}
-	
+
+
 	/**
 	 * Set system-level options
+	 *
 	 * @param sysOpts json array containing system level json elements
 	 */
 	@Step("Set System data.")
 	private void setSystemOptions(JsonObject sysOpts) {
-		//System.out.format("[LOG]: <[%s:%s] SystemData: %s>%n", this.id, this.testName, sysOpts);
+		System.out.format("[LOG]: <[%s:%s] SystemData: %s>%n", this.id, this.testName, sysOpts);
 		sysOpt = sysOpts.get("Opt1") == null ? null : sysOpts.get("Opt1").getAsString(); //example of syntax
 	}
-	
+
+
 	/**
 	 * Given a file path, retrieve test data json array and system options json array
+	 *
 	 * @param jsonDataFilePath
 	 * @return Object[] containing two JSON arrays {testData, systemData}
+	 *
 	 * @throws FileNotFoundException
 	 */
 	private Object[] getTestData(String jsonDataFilePath) throws FileNotFoundException {
@@ -144,6 +156,6 @@ public class DataExtractor {
 		return new Object[] {testsArray, systemArray};
 
 	}
-	
+
 
 }
