c3def54eaa5d109e97d77a4a4293ebdfe0851cb3
jmack-87 <jerimiah.mack@gmail.com>
2018-12-16 20:45:15 -0500

    drop direct file references, provide base paths only
    drop values of unused constants

--- src/test/java/com/jmack/Base/GlobalConstants.java
+++ src/test/java/com/jmack/Base/GlobalConstants.java
@@ -3,11 +3,9 @@ package com.jmack.Base;
 
 public class GlobalConstants {
 
-	protected final String testDataFileName = "TestParametersJSON.json";
-	protected final String testReferenceFileName = "testReference.properties";
-
-	protected final String testDataFilePath = System.getProperty("user.dir") + "\\src\\test\\resources\\" + testDataFileName;
-	protected final String testReferenceFilePath = System.getProperty("user.dir") + "\\src\\test\\resources\\" + testReferenceFileName;
+	protected final String jsonFilesPath = System.getProperty("user.dir") + "\\src\\test\\resources\\jsonData\\";
+	protected final String propertiesFilesPath = System.getProperty("user.dir") + "\\src\\test\\resources\\propertiesData\\";
+	protected final String excelFilesPath = System.getProperty("user.dir") + "\\src\\test\\resources\\excelData\\";
 
 	protected final String locatorSeparator = "@@@";
 	protected final String compoundLocatorPlaceholder = "~~~";
@@ -16,36 +14,41 @@ public class GlobalConstants {
 	protected final String jsonSystemOptionsDataArray = "System Options";
 
 	protected final String perfectoSecurityToken = "";
-	protected final String perfectoHost = "https://ibmgbs.perfectomobile.com/nexperience/perfectomobile/wd/hub";
+	protected final String perfectoHost = "";
 
 	protected final String appiumHost = "http://localhost:4723/wd/hub";
 	protected final String gridHost = "http://localhost:4444/wd/hub";
 
 	protected final String proxy = "";
 
-	//for running local instances
+	/* for running against host machine browsers (non-grid).
+	 * NOTE: 	NOT supported out-of-the-box.
+	 * 			Requires re-write of TestBase "local" configuration to replace grid configurations
+	 */
 	protected final String geckoDriver = "";
+	protected final String firefoxBinary = "";
 	protected final String chromeDriver = "";
 	protected final String ieDriver = "";
 
-	protected final String firefoxBinary = "";
 	protected final String firefoxBrowserLogFile = "target\\fflog.txt";
 
 	protected final int defaultTimeOut = 30;
 	protected final int defaultPollingRate = 1000;
 
+	// DB2 support
 	protected final String jdbcClassName = "";
 	protected final String url = "";
 	protected final String user = "";
 	protected final String password = "";
 
+	// LDAP support
 	protected final String ldapFactory = "";
 	protected final String ldapURL = "";
 	protected final String ldapPass = "";
 	protected final String ldapContext = "";
 
-	// #com.santander.Base.TestBase: SRF login config
-    protected final String srfHost = "https://ftaas.saas.hpe.com:443/wd/hub";
+	// StormRunnerFunctional support
+    protected final String srfHost = "";
     protected final String srfId = "";
     protected final String srfPass = "";
 
