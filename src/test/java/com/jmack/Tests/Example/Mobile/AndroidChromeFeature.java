334e14e3b4a9699c80168a8198628cefad611b5e
Jerimiah Mack <jerimiah.mack@gmail.com>
2019-01-23 09:00:17 -0500

    updates for Ahold-Delhaize demo

--- src/test/java/com/jmack/Tests/Example/Mobile/AndroidChromeFeature.java
+++ src/test/java/com/jmack/Tests/Example/Mobile/AndroidChromeFeature.java
@@ -7,8 +7,8 @@ import org.testng.annotations.Test;
 import com.jmack.Base.TestBase;
 import com.jmack.Base.CustomAnnotations.RetryOnFailCount;
 import com.jmack.Enumerations.Generic;
-import com.jmack.Enumerations.SearchPage;
-import com.jmack.Enumerations.SearchResults;
+import com.jmack.Enumerations.Example.SearchPage;
+import com.jmack.Enumerations.Example.SearchResults;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
@@ -20,7 +20,7 @@ import io.qameta.allure.Story;
 @Epic("Parallelism")
 @Feature("Android Chrome")
 public class AndroidChromeFeature extends TestBase {
-	
+
 	/**
 	 * Perform a google search. Confirm and click first result. Confirm navigation.
 	 * @param testParam optional TestNG value from suite
@@ -33,24 +33,36 @@ public class AndroidChromeFeature extends TestBase {
 	@Parameters({"testParam"})
 	@RetryOnFailCount(0)
 	public void AndroidChromeTest(@Optional String testParam) throws InterruptedException {
-		
+
+		// launch google.com
 		mGeneric.getUrl(Generic.Text_googleURL.toString());
-		
+
+		// confirm google.com is launched
 		mGeneric.confirmElementExistence(SearchPage.Locator_Tag_head.toString());
 		mGeneric.confirmTitle(SearchPage.Text_pageTitle.toString());
-		
-		mGeneric.sendText(SearchPage.Mobile_Locator_TextField_searchInput.toString(), runtimeData.searchString);
-		mGeneric.clickElement(SearchPage.Mobile_Locator_Button_searchSubmit.toString());
-		
-		mGeneric.confirmElementExistence(SearchResults.Mobile_Locator_firstResult.toString());
-		mGeneric.confirmElementExistence(SearchResults.Mobile_CompoundLocator_firstResult.toString(), runtimeData.searchConfirmationString);
-		mGeneric.clickElement(SearchResults.Mobile_Locator_firstResult.toString());
 
-		mGeneric.waitForPageLoaded(30);
-		
-		mGeneric.confirmElementExistence(SearchResults.Locator_Text_ibmSearchConfirmation.toString());
-		
-		//mGeneric.failTest();
+		// set search field
+		mGeneric.sendText(SearchPage.Mobile_Locator_TextField_searchInput.toString(), runtimeData.searchString);
+
+		// submit search
+		mGeneric.clickElement(SearchPage.Mobile_Locator_Button_searchSubmit.toString());
+
+		// confirm at least one result returns
+		mGeneric.confirmElementExistence(SearchResults.Mobile_Locator_firstResult.toString());
+
+		// confirm result is related to query
+		mGeneric.confirmElementExistence(SearchResults.Mobile_CompoundLocator_firstResult.toString(), runtimeData.searchConfirmationString);
+
+		// click result
+		mGeneric.clickElement(SearchResults.Mobile_Locator_Link_firstResult.toString());
+
+		// confirm navigation
+		mGeneric.confirmElementExistence(SearchResults.Mobile_Locator_Text_aholdSearchConfirmation.toString());
+
+		// take screenshot
+		mGeneric.takeScreenShot("Final confirmation.");
+
 	}
+
 
 }
