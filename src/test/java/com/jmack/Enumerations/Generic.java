c319be89b3809303c543b627af4b396b43d15bbe
Jerimiah Mack <jerimiah.mack@gmail.com>
2019-01-23 08:54:04 -0500

    whitespace

--- src/test/java/com/jmack/Enumerations/Generic.java
+++ src/test/java/com/jmack/Enumerations/Generic.java
@@ -1,13 +1,16 @@
 package com.jmack.Enumerations;
 
 public enum Generic {
+
 	Text_googleURL("Generic.Text.googleURL"),
 	Text_nativeContext("Generic.Mobile.Text.Context.NativeApp"),
 	Text_webViewContext("Generic.Mobile.Text.Context.WebView");
+
 
 	private String str;
 	Generic(String value) {str = value;}
 	@Override
 	public String toString() {return str;}
-	
+
+
 }
