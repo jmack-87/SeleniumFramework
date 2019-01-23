ee45767da0e154f105da9cc2550dcef3ee975267
Jerimiah Mack <jerimiah.mack@gmail.com>
2019-01-23 09:14:42 -0500

    add enums

--- src/test/java/com/jmack/Enumerations/Example/SearchResults.java
+++ src/test/java/com/jmack/Enumerations/Example/SearchResults.java
@@ -1,7 +1,7 @@
-package com.jmack.Enumerations;
+package com.jmack.Enumerations.Example;
 
 	public enum SearchResults {
-		
+
 		// DESKTOP
 		Locator_firstResult("SearchResults.Locator.firstResult"),
 		Locator_secondResult("SearchResults.Locator.secondResult"),
@@ -9,14 +9,17 @@ package com.jmack.Enumerations;
 		CompoundLocator_secondResult("SearchResults.CompoundLocator.secondResult"),
 		Locator_Text_ibmSearchConfirmation("SearchResults.Locator.Text.ibmSearchConfirmation"),
 		Locator_Text_santanderSearchConfirmation("SearchResults.Locator.Text.santanderSearchConfirmation"),
-		
+
 		// MOBILE
 		Mobile_Locator_firstResult("SearchResults.Mobile.Locator.firstResult"),
-		Mobile_CompoundLocator_firstResult("SearchResults.Mobile.CompoundLocator.firstResult");
-		
+		Mobile_CompoundLocator_firstResult("SearchResults.Mobile.CompoundLocator.firstResult"),
+		Mobile_Locator_Text_aholdSearchConfirmation("SearchResults.Mobile.Locator.Text.aholdSearchConfirmation"),
+		Mobile_Locator_Link_firstResult("SearchResults.Mobile.Locator.Link.firstResult"),
+		;
+
 		private String str;
 		SearchResults(String value) {str = value;}
 		@Override
 		public String toString() {return str;}
-		
+
 	}
