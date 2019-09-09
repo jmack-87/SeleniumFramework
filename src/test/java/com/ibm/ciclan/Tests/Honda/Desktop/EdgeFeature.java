e6939c3a1ef9ed5bc1934ef16a9fa79467b11bad
Jerimiah Mack <jerimiah.mack@gmail.com>
2019-09-08 20:43:43 -0400

    [multi] begin transition away from com.jmack package to com.ibm.ciclan
    duplicate example artifacts to com.ibm.ciclan, refactor for Honda
    (upcoming client visit)

---
+++ src/test/java/com/ibm/ciclan/Tests/Honda/Desktop/EdgeFeature.java
@@ -0,0 +1,56 @@
+package com.ibm.ciclan.Tests.Honda.Desktop;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.jmack.Base.TestBase;
+import com.jmack.Enumerations.Generic;
+import com.jmack.Enumerations.Example.SearchPage;
+import com.jmack.Enumerations.Example.SearchResults;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
+@Epic("Parallelism")
+@Feature("Edge")
+public class EdgeFeature extends TestBase {
+
+	/**
+	 * Perform a google search. Confirm and click first result. Confirm navigation.
+	 * @param testParam optional TestNG value from suite
+	 * @throws InterruptedException
+	 */
+	@Test(testName="Edge Test",description="Run Edge browser in parallel.")
+	@Severity(SeverityLevel.NORMAL)
+	@Description("Test Description: Run Edge browser in parallel.")
+	@Story("Run Chrome, Firefox, Edge, InternetExplorer in parallel.")
+	@Parameters({"testParam"})
+	public void EdgeTest(@Optional String testParam) throws InterruptedException {
+
+		generic.getUrl(Generic.Text_googleURL.toString());
+
+		generic.confirmElementExistence(SearchPage.Locator_Tag_head.toString());
+		generic.confirmTitle(SearchPage.Text_pageTitle.toString());
+
+		generic.sendText(SearchPage.Locator_TextField_searchInput.toString(), runtimeData.searchString);
+		generic.clickElement(SearchPage.Locator_Button_searchSubmit.toString());
+
+		generic.confirmElementExistence(SearchResults.Locator_firstResult.toString());
+		generic.confirmElementExistence(SearchResults.CompoundLocator_firstResult.toString(), runtimeData.searchConfirmationString);
+		generic.clickElement(SearchResults.Locator_firstResult.toString());
+
+		generic.waitForPageLoaded(30);
+
+		generic.confirmElementExistence(SearchResults.Mobile_Locator_Text_aholdSearchConfirmation.toString());
+
+//		homePagePO.stuff("something passed");
+		generic.takeScreenShot("Final confirmation.");
+
+	}
+
+}
---
+++ src/test/java/com/ibm/ciclan/Tests/Honda/Mobile/IosSafariFeature.java
@@ -0,0 +1,68 @@
+package com.ibm.ciclan.Tests.Honda.Mobile;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.jmack.Base.TestBase;
+import com.jmack.Base.CustomAnnotations.RetryOnFailCount;
+import com.jmack.Enumerations.Generic;
+import com.jmack.Enumerations.Example.SearchPage;
+import com.jmack.Enumerations.Example.SearchResults;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
+@Epic("Parallelism")
+@Feature("iOS Safari")
+public class IosSafariFeature extends TestBase {
+
+	/**
+	 * Perform a google search. Confirm and click first result. Confirm navigation.
+	 * @param testParam optional TestNG value from suite
+	 * @throws InterruptedException
+	 */
+	@Test(testName="Ios Safari Test", description="Run Ios Safari browser in parallel.")
+	@Severity(SeverityLevel.BLOCKER)
+	@Description("Test Description: Run Ios Safari browser.")
+	@Story("Run Ios Safari.")
+	@Parameters({"testParam"})
+	@RetryOnFailCount(0)
+	public void IosSafariTest(@Optional String testParam) throws InterruptedException {
+
+		// launch google.com
+		mGeneric.getUrl(Generic.Text_googleURL.toString());
+
+		// confirm google.com is launched
+		mGeneric.confirmElementExistence(SearchPage.Locator_Tag_head.toString());
+		mGeneric.confirmTitle(SearchPage.Text_pageTitle.toString());
+
+		// set search field
+		mGeneric.sendText(SearchPage.Mobile_Locator_TextField_searchInput.toString(), runtimeData.searchString);
+
+		// submit search
+		mGeneric.clickElement(SearchPage.Mobile_Locator_Button_searchSubmit.toString());
+
+		// confirm at least one result returns
+		mGeneric.confirmElementExistence(SearchResults.Locator_firstResult.toString());
+
+		// confirm result is related to query
+		mGeneric.confirmElementExistence(SearchResults.Mobile_CompoundLocator_firstResult.toString(), runtimeData.searchConfirmationString);
+
+		// click result
+		mGeneric.clickElement(SearchResults.Locator_firstResult.toString());
+
+		// confirm navigation
+		mGeneric.confirmElementExistence(SearchResults.Mobile_Locator_Text_aholdSearchConfirmation.toString(), 90);
+
+		// take screenshot
+		mGeneric.takeScreenShot("Final confirmation.");
+
+	}
+
+
+}
