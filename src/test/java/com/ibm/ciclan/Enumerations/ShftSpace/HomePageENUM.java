8df6c8aa75f7d7acc9de7849ae367e84e594ba10
Matthew Devellis <devellism2@yahoo.com>
2020-04-08 16:00:41 -0400

    Finished the properties for ShftSpace / started filling in ShftSpace.HomePageEnum

--- src/test/java/com/ibm/ciclan/Enumerations/ShftSpace/HomePageENUM.java
+++ src/test/java/com/ibm/ciclan/Enumerations/ShftSpace/HomePageENUM.java
@@ -2,4 +2,16 @@ package com.ibm.ciclan.Enumerations.ShftSpace;
 
 public enum HomePageENUM {
 
+	// DESKTOP
+	Text_URL("ShftSpaceHome.text.url"),
+	Text_PageTitle(),
+	Locator_Button_Register("ShftSpaceHome.Locator.Button.Register"),
+
+	// MOBILE
+
+	private String str;
+    HomePageENUM(String value) {str = value;}
+    @Override
+    public String toString() {return str;}
+
 }
