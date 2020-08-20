6053da16a6cf50a50da7c243bbf2a404ff099346
Matthew Devellis <devellism2@yahoo.com>
2020-08-20 13:05:36 -0400

    Added ShftSpace properties file references to ShftSpace.HomePageEnum

--- src/test/java/com/ibm/ciclan/Enumerations/ShftSpace/HomePageENUM.java
+++ src/test/java/com/ibm/ciclan/Enumerations/ShftSpace/HomePageENUM.java
@@ -4,8 +4,14 @@ public enum HomePageENUM {
 
 	// DESKTOP
 	Text_URL("ShftSpaceHome.text.url"),
-	Text_PageTitle(),
+	Text_PageTitle("ShftSpaceHome.text.pageTitle"),
 	Locator_Button_Register("ShftSpaceHome.Locator.Button.Register"),
+	Locator_Button_Login("ShftSpaceHome.Locator.Button.Login"),
+	Locator_Button_FreeForUpTo5MembersRegister("ShftSpaceHome.Locator.Button.FreeForUpTo5MembersRegister"),
+	Locator_Button_RegisterSmallTeam("ShftSpaceHome.Locator.Button.RegisterSmallTeam"),
+	Locator_Button_RegisterStartup("ShftSpaceHome.Locator.Button.RegisterStartup"),
+	Locator_Button_RegisterAgency("ShftSpaceHome.Locator.Button.RegisterAgency"),
+	Locator_Button_ContactUsEnterprise("ShftSpaceHome.Locator.Button.ContactUsEnterprise");
 
 	// MOBILE
 
