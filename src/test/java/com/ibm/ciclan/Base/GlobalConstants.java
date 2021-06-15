0590991cbe55ad82704256c9b5ff76aaa562a5f5
Jerimiah Mack <jerimiah.mack@gmail.com>
2021-06-15 14:24:15 -0400

    host update

--- src/test/java/com/ibm/ciclan/Base/GlobalConstants.java
+++ src/test/java/com/ibm/ciclan/Base/GlobalConstants.java
@@ -29,8 +29,8 @@ public class GlobalConstants {
 
 	protected final boolean headSpinCapture = true;
 
-	protected final String appiumHost = "http://localhost:4723/wd/hub";
-	protected final String gridHost = "http://localhost:4444/wd/hub";
+	protected final String appiumHost = "http://LAPTOP-ALE01NUB:4723/wd/hub";
+	protected final String gridHost = "http://LAPTOP-ALE01NUB:4444/wd/hub";
 
 	protected final String proxy = "";
 
